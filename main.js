6dfe28a6f8e6a9be3a1ac8d36222a7a9366346a4
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-18 17:16:03 +0000

    completed

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let modal = document.getElementById('modal')
+modal.setAttribute("class", "hidden")
+
+// make event listener for heart click
+
+let heartButtonArray = document.querySelectorAll(".like-glyph")
+
+  heartButtonArray.forEach(heartButton => heartButton.addEventListener("click", (event) => {
+    mimicServerCall("not URL")
+    .then(res => {
+      console.log(res)
+      if (heartButton.innerText === EMPTY_HEART) {
+        heartButton.innerText = FULL_HEART
+        heartButton.className = "activated-heart"
+      } else {
+        heartButton.innerText = EMPTY_HEART
+        heartButton.className = ""
+        console.log(heartButton.className)
+      } 
+    })
+    .catch(reject => {
+      console.log(reject)
+      modal.setAttribute("class", "")
+      setInterval( () => {modal.setAttribute("class", "hidden")}, 3000)
+      if (heartButton.innerText === FULL_HEART) {
+        heartButton.innerText = EMPTY_HEART
+        heartButton.className = ""
+      } else {
+        heartButton.innerText = FULL_HEART
+        heartButton.className = "activated-heart"
+      }
+    })
+  }))
 
 
 
